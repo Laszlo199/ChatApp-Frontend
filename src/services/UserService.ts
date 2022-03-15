f747efb3c8bdff556cff21e5186cdf8b3ed1df66
kubaczechowski <67064580+kubaczechowski@users.noreply.github.com>
2022-03-15 14:36:24 +0100

    small changes

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,14 +1,23 @@
 import axios from "axios";
+import type { UserCredentialsDto } from "@/dtos/userCredentialsDto";
+import type { GetUsersDto } from "@/dtos/GetUsersDto";
 
 export class UserService {
     http = axios.create({
         baseURL: "http://localhost:3001",
         headers: {
-            "content-type": "application/json",
+            "content-type": "application/json"
         },
     });
 
     getAllUsers(id: number) {
         return this.http.get("/users/userId");
     }
+
+
+    async signIn(credentials: UserCredentialsDto): Promise<GetUsersDto> {
+     return  await this.http.post("/users", credentials);
+
+    }
 }
+export default new UserService();
--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -9,13 +9,13 @@
       <form class="text-slate-900 space-y-7 py-2 px-4">
         <div class="">
           <label class="block mb-1 font-bold text-gray-500">Username: </label>
-          <input class="border border-gray-400 p-3 w-full rounded-xl outline-none
+          <input v-model="username" class="border border-gray-400 p-3 w-full rounded-xl outline-none
           focus:border-b-blue-600 border-2" type="text" placeholder="insert username..">
         </div>
 
         <div class="">
           <label class="block mb-1 font-bold text-gray-500">Password: </label>
-          <input class="border border-gray-400 p-3 w-full rounded-xl
+          <input v-model="password" class="border border-gray-400 p-3 w-full rounded-xl
           outline-none focus:border-b-blue-600 border-2" type="password" placeholder="insert password..">
         </div>
 
@@ -24,7 +24,7 @@
           <label  @click="changeSignUp">I don't have an account. Sign up instead!</label>
         </div>
 
-        <button class="block w-full bg-purple-600 py-4 rounded
+        <button @click="signIn" class="block w-full bg-purple-600 py-4 rounded
         text-purple-900 font-bold text-base hover:bg-purple-500">{{signingUp}}</button>
       </form>
 
@@ -34,15 +34,43 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
-
+import { inject, ref } from "vue";
+import type { UserService } from "@/services/UserService";
+import type { UserCredentialsDto } from "@/dtos/userCredentialsDto";
+import type { GetUsersDto } from "@/dtos/GetUsersDto";
+const username = ref("")
+const password = ref("")
 const signingUp = ref("Sign in");
+const userService = inject<UserService>("userService");
+const isLoggedIn = ref(false)
+const loggedUser = ref();
 
 function changeSignUp(){
   if(signingUp.value ==="Sign in")
     signingUp.value = "Sign Up";
   else
     signingUp.value = "Sign in";
+}
+
+function signIn(){
+  if(username.value && password.value){
+
+    console.log(username.value)
+    console.log(password.value)
+
+  userService?.signIn({username: username.value,
+    password: password.value}).then(
+    //value => loggedUser.value = value
+  ).catch((error) => {
+      console.log("error: " + error.message);
+    });
+
+  if(loggedUser.value !==null)
+    console.log("suceess?")
+
+  }
+
+
 }
 
 </script>
